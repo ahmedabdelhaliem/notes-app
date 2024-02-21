e21b33d0731336336d6cd08a222902c431792197
ahmedabdelhaliem <144239916+ahmedabdelhaliem@users.noreply.github.com>
2024-02-21 09:54:23 -0800

    create custom text field

--- lib/widget/add_note_Button_sheet.dart
+++ lib/widget/add_note_Button_sheet.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:untitled8/widget/custom_text_feild.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [const CustomTextField()]),
+      ),
+    );
   }
 }
---
+++ lib/widget/custom_text_feild.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:untitled8/widget/constant.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'title',
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15),
+      borderSide: BorderSide(color: color ?? Colors.white10),
+    );
+  }
+}
